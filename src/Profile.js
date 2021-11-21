7aaeb7958340bb85c1603b537d611f403d8f8797
msantiago9 <marjonsantiago49@gmail.com>
2021-11-20 19:24:11 -0500

    add react-bootstrap styling and mui to profile

--- src/Profile.js
+++ src/Profile.js
@@ -4,6 +4,8 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
+import { InputGroup, FormControl, Button, Col, Row, Container, Stack } from 'react-bootstrap/';
+import { Rating, Avatar } from '@mui/material/';
 import './App.css';
 import propTypes from 'prop-types';
 import { Link, useParams } from 'react-router-dom';
@@ -52,62 +54,93 @@ const Profile = function (props) {
   }
   return (
     <div>
-      <h3>
-        {name}
-        &apos;s Profile Page
-      </h3>
-      <hr />
-      <div>
-        <img src={img} width={400} alt={img} />
-        <p>
-          <b title="TestName">{name}</b>
-        </p>
-        <p>
-          about:
-          {bio}
-        </p>
-        {id === props.userdata.user_id && (
-          <button type="button" onClick={() => setEdit(true)}>
-            click to edit
-          </button>
-        )}
-        {editing && (
-          <div>
-            <input
-              type="text"
-              placeholder={`Edit name: ${name}`}
-              onInput={(text) => setNameField(text.target.value)}
-              maxLength={200}
-            />
-            <br />
-            <input
-              type="text"
-              placeholder={`Edit image: ${img}`}
-              onInput={(text) => setImgField(text.target.value)}
-              maxLength={200}
-            />
-            <br />
-            <input
-              type="text"
-              placeholder={`Edit Bio: ${bio}`}
-              onInput={(text) => setBioField(text.target.value)}
-              maxLength={256}
-            />
-            <br />
-            <button type="button" onClick={() => setEdit(false)}>
-              cancel
-            </button>
-            <button type="button" onClick={() => updateBio()}>
-              submit
-            </button>
-          </div>
-        )}
-      </div>
-      <hr />
-      <div>
-        <h4>- Reviews -</h4>
-        <ReviewList user_id={user_id} reviews={reviews} update={updateReviews} />
-      </div>
+      <Container className="m-4">
+        <Row>
+          <Col md={{ span: 3, offset: 6 }} className="shadow-sm">
+            <p className="h3">{`${name}'s Profile Page`}</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={{ span: 4, offset: 1 }}>
+            <div className="shadow-sm p-3 m-2">
+              <div className="d-flex flex-column justify-content-center">
+                <div className="p-2">
+                  <Avatar
+                    alt={name}
+                    src={img}
+                    sx={{ width: 128, height: 128 }}
+                    style={{ border: '2px solid lightgray' }}
+                    className="shadow"
+                  />
+                </div>
+                <div className="p-2">
+                  <blockquote class="blockquote">
+                    <p class="mb-2">{bio}</p>
+                    <footer class="blockquote-footer">
+                      <cite>
+                        <b title="TestName">{name}</b>
+                      </cite>
+                    </footer>
+                  </blockquote>
+                </div>
+              </div>
+              {id === props.userdata.user_id && (
+                <Button
+                  className="my-3"
+                  variant="primary"
+                  type="button"
+                  onClick={() => setEdit(!editing)}
+                >
+                  Edit Profile
+                </Button>
+              )}
+              {editing && (
+                <div className="shadow-sm">
+                  <InputGroup className="mb-2">
+                    <InputGroup.Text id="NameFormControl">Name</InputGroup.Text>
+                    <FormControl
+                      type="text"
+                      placeholder={name}
+                      aria-label="Name"
+                      aria-describedby="NameFormControl"
+                      onChange={(text) => setNameField(text.target.value)}
+                      maxLength={128}
+                    />
+                  </InputGroup>
+                  <InputGroup className="mb-2">
+                    <InputGroup.Text id="ImgFormControl">Profile Image</InputGroup.Text>
+                    <FormControl
+                      type="text"
+                      placeholder={img}
+                      aria-label="Profile Image"
+                      aria-describedby="ImgFormControl"
+                      onChange={(text) => setImgField(text.target.value)}
+                      maxLength={199}
+                    />
+                  </InputGroup>
+                  <InputGroup className="mb-2">
+                    <InputGroup.Text id="BioFormControl">Personal Bio</InputGroup.Text>
+                    <FormControl
+                      as="textarea"
+                      aria-label="Personal Bio"
+                      placeholder={bio}
+                      onChange={(text) => setBioField(text.target.value)}
+                      maxLength={255}
+                    />
+                  </InputGroup>
+                  <Button variant="success" type="button" onClick={() => updateBio()}>
+                    Submit Changes
+                  </Button>
+                </div>
+              )}
+            </div>
+          </Col>
+          <Col md={{ span: 6, offset: 1 }} className="shadow-sm ml-3">
+            <h4>- Reviews -</h4>
+            <ReviewList user_id={user_id} reviews={reviews} update={updateReviews} />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
@@ -127,28 +160,19 @@ const ReviewList = function (props) {
   }
   const data = JSON.parse(props.reviews);
   return (
-    <div>
+    <Stack gap={3}>
       {data.reviews.map((review) => (
         <div key={review.pokedex_id}>
-          <span>
-            <h3>
-              <Link to={`/pokemon/${review.pokedex_id}`}>{review.title}</Link>
-            </h3>
-            <h5>{`${review.rating} out of 5`}</h5>
-          </span>
-          <span>
-            Pokedex_id:
-            {review.pokedex_id}
-          </span>
-          <span>
-            {' '}
-            by
-            {review.name}
-          </span>
-          <div>{review.body}</div>
+          <Stack direction="horizontal" gap={2}>
+            <Rating name="read-only" value={review.rating} size="small" readOnly />
+            <p className="fw-bold title">
+              <Link to={`/pokemon/${review.pokedex_id}`}>{`${review.title}`}</Link>
+            </p>
+          </Stack>
+          <p>{review.body}</p>
         </div>
       ))}
-    </div>
+    </Stack>
   );
 };
 
