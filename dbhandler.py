77152e6433df66a2d636feac4348db57e2e72388
msantiago9 <marjonsantiago49@gmail.com>
2021-11-24 18:24:55 -0500

    modify populate function, add average rating to pokemon page

--- dbhandler.py
+++ dbhandler.py
@@ -1,15 +1,16 @@
 # pylint: disable=invalid-name
 # pylint: disable=missing-function-docstring
+# pylint: disable=too-many-public-methods
 """POKEREVS"""
+import json
+import secrets
 from random import randint
+from faker import Faker
+import requests
 from werkzeug.security import generate_password_hash
 from sqlalchemy import desc
 import models
 from init import db
-from faker import Faker
-from random import randint
-import requests
-import json
 
 
 class DB:
@@ -45,7 +46,7 @@ class DB:
         return len(models.User.query.filter_by(user_id=user_id).all()) != 0
 
     def isGoogleOnlyUser(email):
-        return models.User.query.filter_by(email=email).first().password == None
+        return models.User.query.filter_by(email=email).first().password is None
 
     def isUserByEmail(email):
         return len(models.User.query.filter_by(email=email).all()) != 0
@@ -92,7 +93,9 @@ class DB:
             )
         output = ""
         for pokemon in ratings:
-            output = f"{output}\n{pokemon['pokedex_id']}. average: {pokemon['average']} out of 5 ({pokemon['all_scores']})"
+            output = f"""{output}\n{pokemon['pokedex_id']}.
+             average: {pokemon['average']} out of 5 
+            ({pokemon['all_scores']})"""
         print(output)
 
     def getPokemonReviews(pokedex_id):
@@ -132,6 +135,7 @@ class DB:
         db.session.add(review)
         db.session.commit()
 
+    # pylint: disable=useless-return
     def deleteReview(user_id, pokedex_id):
         review = models.Review.query.filter_by(
             user_id=user_id, pokedex_id=pokedex_id
@@ -219,6 +223,7 @@ class DB:
     def jsonifyTopReviews(reviews):
         data = {}
         data["reviews"] = []
+        # pylint: disable=not-an-iterable
         for review in reviews:
             data["reviews"].append(
                 {
@@ -237,20 +242,166 @@ class DB:
         user_list = []
         pokemon_list = []
         user_count = 100
-        max_pokedex_id = 850
-        min_reviews_per_pokemon = 2
-        max_reviews_per_pokemon = 10
+        max_pokedex_id = 890
+        min_reviews_per_pokemon = 4
+        max_reviews_per_pokemon = 12
+        positive_adverbs = [
+            'boldly',
+            'bravely',
+            'brightly',
+            'cheerfully',
+            'deftly',
+            'devotedly',
+            'eagerly',
+            'elegantly',
+            'faithfully',
+            'fortunately',
+            'gleefully',
+            'gracefully',
+            'happily',
+            'honestly',
+            'innocently',
+            'kindly',
+            'merrily',
+            'obediently',
+            'perfectly',
+            'politely',
+            'powerfully',
+            'safely',
+            'victoriously',
+            'warmly',
+            'vivaciously',
+        ]
+
+        negative_adverbs = [
+            "achingly",
+            "angrily",
+            "annoyingly",
+            "anxiously",
+            "badly",
+            "boastfully",
+            "dejectedly",
+            "enviously",
+            "foolishly",
+            "hopelessly",
+            "irritably",
+            "jealously",
+            "joylessly",
+            "lazily",
+            "miserably",
+            "morosely",
+            "obnoxiously",
+            "painfully",
+            "poorly",
+            "rudely",
+            "sadly",
+            "selfishly",
+            "terribly",
+            "unhappily",
+            "wearily",
+        ]
+
+        neutral_adverbs = [
+            "accidentally",
+            "awkwardly",
+            "blindly",
+            "coyly",
+            "crazily",
+            "defiantly",
+            "deliberately",
+            "doubtfully",
+            "dramatically",
+            "dutifully",
+            "enormously",
+            "excitedly",
+            "hungrily",
+            "mortally",
+            "mysteriously",
+            "nervously",
+            "seriously",
+            "shakily",
+            "restlessly",
+            "solemnly",
+            "sternly",
+            "unexpectedly",
+            "wildly",
+        ]
+        positive_adjectives = [
+            'affectionate',
+            'agreeable',
+            'amiable',
+            'bright',
+            'charming',
+            'creative',
+            'determined',
+            'diligent',
+            'diplomatic',
+            'dynamic',
+            'energetic',
+            'friendly',
+            'funny',
+            'generous',
+            'giving',
+            'gregarious',
+            'hardworking',
+            'helpful',
+            'imaginative',
+            'kind',
+            'likable',
+            'loyal',
+            'patient',
+            'polite',
+            'sincere',
+        ]
+
+        negative_adjectives = [
+            'arrogant',
+            'obstinate',
+            'moody',
+            'impolite',
+            'sneaky',
+            'big-headed',
+            'resentful',
+            'cruel',
+            'machiavellian',
+            'aloof',
+            'pompous',
+            'cowardly',
+            'intolerant',
+            'silly',
+            'overcritical',
+            'dishonest',
+            'finicky',
+            'possessive',
+            'untrustworthy',
+            'secretive',
+            'domineering',
+            'inflexible',
+            'tactless',
+            'unreliable',
+            'inconsiderate',
+            'hostile',
+            'deceitful',
+            'stubborn',
+            'impatient',
+            'careless',
+            'foolish',
+            'touchy',
+            'vulgar',
+        ]
+
         for _ in range(user_count):
             current = fake.profile()
             email = current["mail"]
             password = generate_password_hash("123456", method="sha256")
             name = current["name"]
-            img = "https://static.wikia.nocookie.net/pokemon/images/5/57/Red_FireRed_and_LeafGreen.png"
-            bio = f"Day time {current['job']} at {current['company']}, night time reviewer. {fake.sentence(nb_words=8, variable_nb_words=False)}"
+            img = "https://i.imgur.com/DFCgM3b.jpg"
+            bio = f"""Day time {current['job']} at {current['company']}, night time reviewer.
+            \n{fake.sentence(nb_words=8, variable_nb_words=False)}"""
 
             choice = randint(0, 2)
             if choice == 0:
-                img = "https://cdn2.bulbagarden.net/upload/6/6f/Black_White_Hilda.png"
+                img = "https://i.imgur.com/yivQc38.jpg"
                 name = f"{name} {fake.suffix_nonbinary()}"
             elif choice == 1:
                 api_img_url = "http://aws.random.cat/meow"
@@ -264,15 +415,67 @@ class DB:
             )
 
         for i in range(1, max_pokedex_id + 1):
-            num_reviews = randint(min_reviews_per_pokemon, max_reviews_per_pokemon)
+            num_reviews = randint(min_reviews_per_pokemon,
+                                  max_reviews_per_pokemon)
             pokemon_list.append(models.Pokemon(pokedex_id=i))
             for _ in range(1, num_reviews):
                 rand_user = randint(0, user_count - 1)
-                rand_rating = randint(0, 5)
+                rand_rating = randint(1, 5)
+                if rand_rating > 3:
+                    adverb = secrets.choice(positive_adverbs)
+                    adjective = secrets.choice(positive_adjectives)
+                    start = secrets.choice([
+                        f"""I had a great time working with this Pokemon.
+                         It really performs very {adverb}.
+                        \n\nVery {adjective} around other Pokemon,
+                         making it flexible for the team.""",
+                        f"""I can't believe I've lived without this one for so long!
+                        \nIt does the chores around the house incredibly {adverb}.
+                         You have to try it out!""",
+                        f"""Underrated Pokemon. After spending some time with it,
+                         you'll find out how {adverb} it fights.
+                         A very {adjective} species to top it off!!!""",
+                        f"""It's all been said before, don't only take my word for it. 
+                         You won't regret it. {adjective} af.""",
+                    ])
+                elif rand_rating < 3:
+                    adverb = secrets.choice(negative_adverbs)
+                    adjective = secrets.choice(negative_adjectives)
+                    start = secrets.choice([
+                        f"""Before you buy this one, I suggest you look around first.
+                         I didn't know it worked {adverb} around other Pokemon.
+                         Starts are nice, but personality is a 0.""",
+                        f"""Overrated Pokemon. When I'm at home it behaves fine, 
+                         but otherwise it's {adjective} and that causes a
+                         lot of problems. Would not recommend.""",
+                        f"""Before you scroll away, there is a reason for the score--
+                        It plays very {adverb} in practice.
+                         Can't say that bodes well for the future.""",
+                        f"""One to describe this Pokemon: {adjective}.
+                        \nIt's a deal-breaker for me.
+                         If you can tolerate that, then I say go for it.""",
+                        """Works for the first 3 months, then it broke.
+                         Seeing if I can get my Pokédollars back.""",
+                    ])
+                else:
+                    adverb = secrets.choice(neutral_adverbs)
+                    start = secrets.choice([
+                        f"""It just werks. Doesn't stand out but still decent.
+                         It likes to act {adverb}.""",
+                        f"""HIGHLY overrated, but not entirely unusable.
+                         I say, go for it. Be careful though,
+                         sometimes it likes to stand around {adverb}.""",
+                        f"""Do more research before selecting this one folks!
+                         It doesn't work everywhere,
+                         especially since {adverb} runs away from time to time.
+                        \nThat being said, It works in a few of my teams.""",
+                        "Meh.",
+                    ])
                 review = models.Review(
                     rating=rand_rating,
-                    title=f"Review of pokemon #{i} - {fake.sentence(nb_words=4, variable_nb_words=False)}",
-                    body=fake.sentence(nb_words=20, variable_nb_words=False),
+                    title=f"""Review of Pokemon #{i}
+                     - {fake.sentence(nb_words=4, variable_nb_words=False)}""",
+                    body=f"{start}\n\n{fake.sentence(nb_words=20, variable_nb_words=False)}",
                 )
                 user_list[rand_user].reviews.append(review)
                 pokemon_list[i - 1].reviews.append(review)
